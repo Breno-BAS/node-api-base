b02c427a6f91b94f25a584c4356aae4c87df3248
Breno Andrade <engbrenoandrade@gmail.com>
2026-03-10 19:40:12 -0300

    gerando timestamp padrão

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,9 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+const timestamps = {
+    createdAt: timestamp().defaultNow(),
+    updatedAt: timestamp().defaultNow().notNull()
+}
 
 export const usersTable = pgTable('users', {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,11 +11,13 @@ export const usersTable = pgTable('users', {
     age: integer().notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
     obs: varchar({ length: 255 }),
-    balance: integer().default(100)
+    balance: integer().default(100),
+    ...timestamps
 });
 
 export const pestsTable = pgTable('pests', {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
-    ownerId: integer().notNull().references(() => usersTable.id)
+    ownerId: integer().notNull().references(() => usersTable.id),
+    ...timestamps
 }); // Relação 1:N
