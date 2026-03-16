387be72258156e81e0b43159edbd022dde449c4d
Breno Andrade <engbrenoandrade@gmail.com>
2026-03-16 16:50:34 -0300

    Fazendo autenticação Basic Auth.
    -- Utiliza o encoded e o headers no insomnia

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,9 +1,33 @@
 import { Request, Response, NextFunction } from 'express';
+import { usuarioTable } from '../db/schema'
+import { db } from '../libs/drizzle';
+import { eq, and } from 'drizzle-orm';
 
 export const Auth = {
-    private: (req: Request, res: Response, next: NextFunction) => {
-        // Fazer verificação de auth
-        let success = true; 
+    private: async (req: Request, res: Response, next: NextFunction) => {
+        let success = false; 
+        
+        if(req.headers.authorization) {
+            let hash: string = req.headers.authorization.substring(6);
+            let decoded: string = Buffer.from(hash, 'base64').toString();
+            let data: string[] = decoded.split(':');
+
+            if(data.length === 2) {
+                let hasUser = await db
+                .select()
+                .from(usuarioTable)
+                .where(
+                    and(
+                        eq(usuarioTable.email, data[0]),
+                        eq(usuarioTable.password, data[1])
+                    )
+                );
+                if(hasUser) {
+                    success = true;
+                }
+            }
+        };
+
 
         if(success) {
             next();
